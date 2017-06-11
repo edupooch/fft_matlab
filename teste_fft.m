f9cf98cf5d9e9ff4716b445887db281d8254a5c3
Eduardo H P Pooch <edupooch@gmail.com>
2017-06-11 16:42:00 -0300

    Create teste_fft.m

--- teste_fft.m
+++ teste_fft.m
@@ -1,5 +1,5 @@
 % taxa de amostragem 4000 amostras por segundo
-% media variancia e autocorrelação dos trechos de 10 em 10 QRS
+% media variancia e autocorrelaÃ§Ã£o dos trechos de 10 em 10 QRS
 sinal = importdata('teste.txt');
 sinal = sinal(1:16384);
 sinal = sinal.'; %inverte linhas e colunas
@@ -8,12 +8,12 @@ len = length(sinal);
 fsample = 4000;
 res = fsample/len;
 
-Y = fft(sinal);
+Y = my_fft(sinal);
 
 Pyy = Y.*conj(Y)/len; %para representar num complexos
 f = res*(0:(len/2)-1);
 figure; plot(f,Pyy(1:len/2));
-title('Densidade espectral'); xlabel('Frequência (Hz)'); ylabel('Amplitude')
+title('Densidade espectral'); xlabel('FrequÃªncia (Hz)'); ylabel('Amplitude')
 
 figure; plot(f(1:500),Pyy(1:500));
-title('Densidade espectral ZOOM'); xlabel('Frequência (Hz)');  ylabel('Amplitude')
+title('Densidade espectral ZOOM'); xlabel('FrequÃªncia (Hz)');  ylabel('Amplitude')
